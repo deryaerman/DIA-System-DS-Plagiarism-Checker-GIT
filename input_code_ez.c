527e542cf0173b4f1235e262565f6612135c2452
deryaerman <erman@tu-berin.de>
2025-01-28 15:54:16 +0100

    local/global var logic + typeless declerations

--- input_code_ez.c
+++ input_code_ez.c
@@ -1,5 +1,14 @@
-float func() {
+int main() {
     int a = 0;
-    float res = (global1 % global2) * init(global1, global2); 
-    return res; 
+   
+
+    float res = (a + c); 
+
+    float func() {
+        b = b += 1;
+
+        return b; // Return b to the calling process
+    }
+
+    return 0; // Return 0 to indicate successful execution
 }
