430f6f20a18152b997b2615637e569094220574a
mariaslowinska <m.slowinska@campus.tu-berlin.de>
2025-01-29 23:20:40 +0100

    without head!!!!

--- input_code_ez.c
+++ input_code_ez.c
@@ -1,31 +1,7 @@
-<<<<<<< HEAD
 int summation(int start_val,int end_val) { 
     for (int i = start_val; i <= end_val; i++) {
         sum += i;
     } 
     return result; // Return 0 to indicate successful execution
-=======
-// int summation(int start_val,int end_val) { 
-//     int sum= 0; 
-
-//     void hallo (int b) {
-//         sum = b + c;
-
-//     }
-
-    
-//     return sum; 
-// }
-int main() {
-
-    for (int i = 0; i < 10; i++){
-        sum = i + sum;
-    }
-    
-    return sum; // Return 0 to indicate successful execution
->>>>>>> 6481511b986874c6c1f0a1aafdc43e3601cbded9
 } 
-
-
-
 
